be4d29f61630d56832199c4aa263b24c8662939f
gcancer <wldyd0721@naver.com>
2020-08-21 23:54:36 +0900

    break_continue, if, for

---
+++ com/control/break_continue.py
@@ -0,0 +1,22 @@
+i = 1
+
+while i <= 10:
+    if i > 5:
+        break
+    print(i)
+    i += 1
+else:
+    print('i',i,sep='=')
+
+print()
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+else:
+    print('i',i,sep='=')
+
+###############
+# while의 실행 결과 : 1 2 3 4 5
+# for의 실행 결과 : 1 3 5 7 9 i=10
